e9acf40580f786a210b5fb5a05aeb4cd5463db5f
theo dubocq <theo.dubocq@gmail.com>
2023-04-02 11:40:51 +0200

    Push final , correction font et css error

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { Routes, Route,BrowserRouter,Navigate } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import './App.css';
-import Accueil from "./pages/Accueil/Accueil";
-import Fiche from "./pages/Fiche-logement/FicheLogement";
-import APropos from "./pages/A-propos/APropos";
-import Erreur404 from "./pages/404/404";
+import RoutesPath from "./routes/RoutesPath";
 
 function App() {
     return (
@@ -13,13 +10,7 @@ function App() {
       <BrowserRouter>
         <Header />
 			    <main>
-            <Routes>
-              <Route element={<Navigate replace to="/accueil" />} path="/" />
-              <Route path="/accueil" element={<Accueil />}/>
-              <Route path="/logement/:id" element={<Fiche />}/>
-              <Route path="/a-propos" element={<APropos />}/>
-              <Route path="*" element={<Erreur404 />}/>
-            </Routes>
+           <RoutesPath/>
          </main>
         <Footer />
       </BrowserRouter>
--- src/routes/RoutesPath.js
+++ src/routes/RoutesPath.js
@@ -8,13 +8,13 @@ import Erreur404 from "../pages/404/404";
 
 function RoutesPath() {
     return(
-                <Routes>
-                    <Route element={<Navigate replace to="/accueil" />} path="/" />
-                    <Route path="/accueil" element={<Accueil />}/>
-                    <Route path="/logement/:id" element={<Fiche />}/>
-                    <Route path="/a-propos" element={<APropos />}/>
-                    <Route path="*" element={<Erreur404 />}/>
-                </Routes>
+        <Routes>
+        <Route element={<Navigate replace to="/accueil" />} path="/" />
+        <Route path="/accueil" element={<Accueil />}/>
+        <Route path="/logement/:id" element={<Fiche />}/>
+        <Route path="/a-propos" element={<APropos />}/>
+        <Route path="*" element={<Erreur404 />}/>
+      </Routes>
     );
 }
 
